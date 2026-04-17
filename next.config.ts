fe879b37d589ee185367d78d54728d788a83942f
jerryjohnsoffl <118886936+jerryjohnsoffl@users.noreply.github.com>
2026-04-18 03:00:20 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  distDir: "out",
   basePath: "/arena-insight",
   images: {
     unoptimized: true,
